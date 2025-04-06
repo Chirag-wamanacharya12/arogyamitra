f75448bffa7fec97cbc972278ef5755f8c7dc9c1
Chirag-wamanacharya12 <chiragwamanacharya42@gmail.com>
2025-04-07 01:34:39 +0530

    Update settings.py

--- arogyaMitra/settings.py
+++ arogyaMitra/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,14 +134,7 @@ SOCIALACCOUNT_ADAPTER = "userprofile.adapters.CustomSocialAccountAdapter"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'healthcare',
-        'USER':'root',
-        'PASSWORD':'Chirag@root#12',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
